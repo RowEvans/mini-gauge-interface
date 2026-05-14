2158256474369ec380213e9a49ed6da12d8d2f94
Rowan Evans <raevans2009@gmail.com>
2026-05-14 15:33:13 -0400

    added a fuel gauge bar

--- main.py
+++ main.py
@@ -77,54 +77,86 @@ def drawSpeedometer(surface, speed):
 MIN_RPM = 0
 MAX_RPM = 8000
 REDLINE_RPM = 7000
-BAR_WIDTH = 20
-BAR_HEIGHT = 400
-BAR_X = 100
-BAR_Y = 100
+RPM_WIDTH = 20
+RPM_HEIGHT = 400
+RPM_X = 100
+RPM_Y = 100
 TICK_WIDTH = 10
 
 def drawTachometer(surface, rpm):
-    pygame.draw.rect(surface, white, (BAR_X, BAR_Y, BAR_WIDTH, BAR_HEIGHT))
+    pygame.draw.rect(surface, white, (RPM_X, RPM_Y, RPM_WIDTH, RPM_HEIGHT))
 
     #drawing redline area
-    redline_fill = int(BAR_HEIGHT * (1300 / 8000))
-    pygame.draw.rect(surface, red, (BAR_X, BAR_Y, BAR_WIDTH, redline_fill))
+    redline_fill = int(RPM_HEIGHT * (1300 / 8000))
+    pygame.draw.rect(surface, red, (RPM_X, RPM_Y, RPM_WIDTH, redline_fill))
 
     ratio = rpm / MAX_RPM
-    fill_height = int(BAR_HEIGHT * ratio)
-    fill_y = BAR_Y + BAR_HEIGHT - fill_height
+    fill_height = int(RPM_HEIGHT * ratio)
+    fill_y = RPM_Y + RPM_HEIGHT - fill_height
 
     if rpm >= REDLINE_RPM:
         color = red
     else:
         color = green
     
-    pygame.draw.rect(surface, color, (BAR_X, fill_y, BAR_WIDTH, fill_height))
+    pygame.draw.rect(surface, color, (RPM_X, fill_y, RPM_WIDTH, fill_height))
 
     #drawing tick marks
     for i in range(0, MAX_RPM, 1000):
         ratio = i / MAX_RPM
-        tick_height = int(BAR_HEIGHT * ratio)
-        tick_y = BAR_Y + BAR_HEIGHT - tick_height
+        tick_height = int(RPM_HEIGHT * ratio)
+        tick_y = RPM_Y + RPM_HEIGHT - tick_height
 
-        pygame.draw.line(surface, black, (BAR_X, tick_y), (BAR_X + TICK_WIDTH, tick_y))
+        pygame.draw.line(surface, black, (RPM_X, tick_y), (RPM_X + TICK_WIDTH, tick_y))
         
         tick_label = font.render(f"{(i // 1000)}", True, white)
-        tick_rect = tick_label.get_rect(center=((BAR_X - 15), tick_y))
+        tick_rect = tick_label.get_rect(center=((RPM_X - 15), tick_y))
         surface.blit(tick_label, tick_rect)
 
     title = font.render("RPM", True, white)
-    title_rect = title.get_rect(center=((BAR_X + BAR_WIDTH // 2), (BAR_Y - 20)))
+    title_rect = title.get_rect(center=((RPM_X + RPM_WIDTH // 2), (RPM_Y - 20)))
     surface.blit(title, title_rect)
 
     rpm_label = font.render(f"{rpm}", True, white)
-    rpm_rect = rpm_label.get_rect(center=((BAR_X + BAR_WIDTH //2), (BAR_Y + BAR_HEIGHT + 20)))
+    rpm_rect = rpm_label.get_rect(center=((RPM_X + RPM_WIDTH //2), (RPM_Y + RPM_HEIGHT + 20)))
     surface.blit(rpm_label, rpm_rect)
 
+#fuel gauge
+MIN_FUEL = 0
+MAX_FUEL = 13.2
+FUEL_X = 650
+FUEL_Y = 100
+FUEL_WIDTH = 20
+FUEL_HEIGHT = 400
+
+def drawFuelGauge(surface, fuel):
+    pygame.draw.rect(surface, white, (FUEL_X, FUEL_Y, FUEL_WIDTH, FUEL_HEIGHT))
+
+    ratio = fuel / MAX_FUEL
+    fill_height = int(FUEL_HEIGHT * ratio)
+    fill_y = FUEL_Y + FUEL_HEIGHT - fill_height
+
+    pygame.draw.rect(surface, green, (FUEL_X, fill_y, FUEL_WIDTH, fill_height))
+
+    for i in range(0, int(MAX_FUEL), 3):
+        ratio = i / MAX_FUEL
+        tick_height = int(FUEL_HEIGHT * ratio)
+        tick_y = FUEL_Y + FUEL_HEIGHT - tick_height
+
+        pygame.draw.line(surface, black, (FUEL_X, tick_y), (FUEL_X + TICK_WIDTH, tick_y))
+
+    empty = font.render("E", True, white)
+    empty_rect = empty.get_rect(center=((FUEL_X - 10), (FUEL_Y + FUEL_HEIGHT)))
+    surface.blit(empty, empty_rect)
+
+    full = font.render("F", True, white)
+    full_rect = full.get_rect(center=((FUEL_X - 10), (FUEL_Y)))
+    surface.blit(full, full_rect)
 
 
 
 #defaults / debug
+fuel = 0
 rpm = 0
 speed = 0
 flip = 1
@@ -142,6 +174,7 @@ while running:
     screen.fill(black)
     drawSpeedometer(screen, speed)
     drawTachometer(screen, rpm)
+    drawFuelGauge(screen, fuel)
     if debug_mode:
         drawGrid(screen)
         trackMouse(screen)
@@ -155,5 +188,6 @@ while running:
     
     speed += flip
     rpm += flip * 60
+    fuel += flip * 0.1
 
 pygame.quit()
