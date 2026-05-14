5d739f0f21c838b92f3eb54646e8058883855738
Rowan Evans <raevans2009@gmail.com>
2026-05-14 00:59:13 -0400

    inital pygame test -> beginning to learn

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 import pygame
 
-print ('Hello world.')
+screen = pygame.display.set_mode((1080, 800))
+pygame.display.set_caption("Main Page")
+
+black = (0, 0, 0)
+white = (255, 255, 255)
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill(black)
+    pygame.draw.circle(screen, white, (540, 400), 200, 5)
+    pygame.display.flip()
+
+pygame.quit()
